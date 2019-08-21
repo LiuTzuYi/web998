728cc5ff17b2ea7954bb45ca7a5c74b9c9ebd5eb
liutzuyi <liutzuyi@gmail.com>
2019-08-21 23:57:05 +0800

    模組 web998 的初始簽入

---
+++ web998/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web998.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
